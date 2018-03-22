869e2aa0d6c2e19261cd13a6c8088026fa9c48bc
helloworld575 <zhichenli6@gmail.com>
2018-03-22 20:09:05 +0800

    been driven crazy

--- change_seat.py
+++ change_seat.py
@@ -48,7 +48,23 @@ class Connect_database:
         self.insert_data(5, "Jeams")
 
     def handle_db(self):
-        sql = 'select * from seat s1 WHERE s1.id%2=0'
+        # sql1 = 'create table seat1 (id int PRIMARY KEY NOT NULL ,student NOT NULL)'
+        # self.cur.execute(sql1)
+        # self.con.commit()
+        # sql2 = 'insert into seat1 SELECT * FROM seat'
+        # self.cur.execute(sql2)
+        # self.con.commit()
+        sql3 = '''update seat set student = CASE id 
+              WHEN id%2=0 THEN (SELECT student FROM seat1 WHERE seat1.id=seat.id-1) 
+              WHEN id%2=1 THEN (SELECT student FROM seat1 WHERE seat1.id=seat.id+1) 
+              END 
+              '''
+        self.cur.execute(sql3)
+        self.con.commit()
+        sql4 = 'select student from seat1 s1 where s1.id%2=0'
+        self.cur.execute(sql4)
+
+        print(self.cur.fetchall())
     def close_db(self):
         self.con.close()
 
@@ -56,3 +72,35 @@ class Connect_database:
 if __name__ == '__main__':
     C = Connect_database()
     C.create_db()
+    C.handle_db()
+'''
+    fail to solve,find answer from web:
+    
+    SELECT
+    s.id,
+    s.student
+FROM
+    (
+        SELECT
+            id - 1 AS id,
+            student
+        FROM
+            seat
+        WHERE
+            (id % 2 = 0)
+        UNION
+            SELECT
+                (CASE WHEN (cnt%2=1) AND id=cnt THEN id ELSE id + 1 END) AS id,
+                student
+            FROM
+                seat,
+                (select count(*) as cnt from seat) as seatcnt
+            WHERE
+                (id % 2 = 1)
+    ) s
+GROUP BY
+    s.id ASC
+    
+well,wrong at first
+ps: I still don't know reason why this is right answer
+'''
